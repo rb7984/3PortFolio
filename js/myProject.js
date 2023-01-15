85902a4566938bc2d047e26bc331a2426d91350f
riccbarelli <99691825+rb7984@users.noreply.github.com>
2023-01-15 18:00:14 +0100

    camera orient problem

--- js/myProject.js
+++ js/myProject.js
@@ -1,8 +1,8 @@
 import * as THREE from './three.module.js';
-import {OrbitControls} from './OrbitControls.js';
 import {FBXLoader} from './FBXLoader.js';
-import {InteractionManager} from './three.interactive.js';
 import {gsap} from './gsap/index.js';
+import {OrbitControls} from './OrbitControls.js';
+// import {InteractionManager} from './three.interactive.js';
 
 const container = document.querySelector('#scene-container');
 export const scene = new THREE.Scene();
@@ -10,9 +10,13 @@ export const scene = new THREE.Scene();
 var clock = new THREE.Clock();
 
 const camera = new THREE.PerspectiveCamera(
-    45, window.innerWidth / window.innerHeight, 0.1, 1000 );
+    45,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+);
 
-camera.position.set(-80,60,-80);
+camera.position.set(50,50,50);
 camera.lookAt(0,0,0);
 
 const renderer = new THREE.WebGLRenderer();
@@ -33,11 +37,13 @@ function onWindowResize(){
     renderer.setSize( window.innerWidth, window.innerHeight );
 }
 
-const interactionManager = new InteractionManager(
-    renderer,
-    camera,
-    renderer.domElement
-);
+//INTERACTIONS
+
+// const interactionManager = new InteractionManager(
+//     renderer,
+//     camera,
+//     renderer.domElement
+// );
 
 var g2 = new THREE.PlaneBufferGeometry(2000, 2000, 8, 8);
 var m2 = new THREE.MeshStandardMaterial({ color: '#2c61d4', side: THREE.DoubleSide });
@@ -45,10 +51,9 @@ var plane = new THREE.Mesh(g2, m2);
 plane.rotateX( - Math.PI / 2);
 plane.translateY(1);
 plane.receiveShadow = true;
-
 scene.add(plane);
 
-//Lights
+//LIGHTS
 
 const light = new THREE.AmbientLight( 0xdedede, 1.5);
 light.castShadow = true;
@@ -60,29 +65,29 @@ lightPoint.lookAt(0, 0, 0);
 lightPoint.castShadow = true;
 scene.add(lightPoint);
 
-// const lightHelper = new THREE.PointLightHelper(lightPoint,2,'#a18787')
-// scene.add(lightHelper)
-
-//Controls
+//CONTROLS
 
 const controls = new OrbitControls(camera, renderer.domElement);
-const cPX = [50,-60,-50,50];
-const cPY = [50,10,50,50];
-const cPZ = [50,-20,-50,-50];
-const cT = [[-100,6,70],[-100,6,70],[-100,6,70],[0,0,0]]
+// const cPX = [50,-50,-50,50];
+// const cPY = [50,50,50,50];
+// const cPZ = [50,50,-50,-50];
+
+const cP = [[50,50,50],[-60,7,-20],[-50,50,-50],[50,50,-50]]
+const cT = [[0,0,0],[-40,6,-70],[0,0,0],[0,0,0]]
 var cPC = 0;
 
 function ChangeView(a) {
-    if (a) cPC += 1;
-    else cPC -= 1;
+    if (a) {cPC += 1;}
+    else {cPC -= 1;}
     
     gsap.core.Tween.to(camera.position, {
-            x: cPX[cPC%4],
-            y: cPY[cPC%4],
-            z: cPZ[cPC%4],
-            duration: 1,
-        })
-    camera.lookAt(-100,6,70);
+        x: cP[cPC%4][0],
+        y: cP[cPC%4][1],
+        z: cP[cPC%4][2],
+        duration: 1
+    });
+
+    camera.lookAt(cT[cPC%4][0],cT[cPC%4][1],cT[cPC%4][2]);
 }
 
 document.getElementById('previous').onclick = function () {
@@ -146,18 +151,15 @@ function UpdateCamera() {
     camera.lookAt(0, 0, 0);
 }
 
-function CameraTransition(a) {
-
-
-}
-
 function animate() {
 	requestAnimationFrame( animate );
     
-    interactionManager.update();
+    //INTERACTIONS
+    // interactionManager.update();
+    //CONTROLS
     controls.update();
     
-    if (cPC == 0) UpdateCamera();
+    if (cPC == 0) {UpdateCamera();}
     
     renderer.render( scene, camera );
 };
